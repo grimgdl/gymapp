fc0c9eaf50b139bd9941f324abe0159a48ad13d8
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2025-12-04 18:25:57 -0600

    text size improvements

--- composeApp/src/commonMain/kotlin/com/grimco/gymapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/grimco/gymapp/App.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.grimco.gymapp.model.TrainingEntity
 import com.grimco.gymapp.presenter.components.FavoriteList
 import com.grimco.gymapp.presenter.components.Stats
@@ -19,16 +23,31 @@ import com.grimco.gymapp.ui.theme.MaterialGym
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview(showBackground = true, )
+@Preview(showBackground = true)
 fun App() {
     MaterialGym(
     ) {
 
-        val favoriteList = remember { listOf(
-            TrainingEntity(id = 1, name = "Leg", image = "https://cdn.shopify.com/s/files/1/0568/6280/2107/files/fentes_bulgares_600x600.jpg?v=1705571601"),
-            TrainingEntity(id = 3, name = "functional", image = "https://cdn.shopify.com/s/files/1/0568/6280/2107/files/hip_trust_600x600.jpg?v=1705571653"),
-            TrainingEntity(id = 4, name = "Speed", image = "https://res.cloudinary.com/display97/image/upload/87196.jpg"),
-        ) }
+
+        val favoriteList = remember {
+            listOf(
+                TrainingEntity(
+                    id = 1,
+                    name = "Leg",
+                    image = "https://cdn.shopify.com/s/files/1/0568/6280/2107/files/fentes_bulgares_600x600.jpg?v=1705571601"
+                ),
+                TrainingEntity(
+                    id = 3,
+                    name = "functional",
+                    image = "https://cdn.shopify.com/s/files/1/0568/6280/2107/files/hip_trust_600x600.jpg?v=1705571653"
+                ),
+                TrainingEntity(
+                    id = 4,
+                    name = "Speed",
+                    image = "https://res.cloudinary.com/display97/image/upload/87196.jpg"
+                ),
+            )
+        }
 
         Column(
             modifier = Modifier
@@ -38,8 +57,14 @@ fun App() {
         ) {
 
             Text(
-                text = "favorite"
+                text = "favorite",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
             )
+            Spacer(modifier = Modifier.height(15.dp))
 
             FavoriteList(
                 list = favoriteList
@@ -48,7 +73,13 @@ fun App() {
             Spacer(modifier = Modifier.height(15.dp))
 
             Text(
-                text = "Stats"
+                text = "Stats",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+
+                )
             )
 
             Spacer(modifier = Modifier.height(15.dp))
