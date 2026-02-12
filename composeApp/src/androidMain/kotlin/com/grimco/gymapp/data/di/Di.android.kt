2b1bade57d93f87b22b066d1ffb4d57c8bbe2bfd
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2026-02-11 18:35:01 -0600

    add viewmodel di implementation

--- composeApp/src/androidMain/kotlin/com/grimco/gymapp/data/di/Di.android.kt
+++ composeApp/src/androidMain/kotlin/com/grimco/gymapp/data/di/Di.android.kt
@@ -2,7 +2,11 @@ package com.grimco.gymapp.data.di
 
 import androidx.room.RoomDatabase
 import com.grimco.gymapp.data.local.AppDatabase
+import com.grimco.gymapp.data.service.TrainingService
+import com.grimco.gymapp.presentation.viewmodel.MainViewmodel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 
@@ -11,4 +15,7 @@ actual val platformModule: Module = module {
     single<RoomDatabase.Builder<AppDatabase>> {
         getDatabaseBuilder(get())
     }
+
+    viewModelOf(::MainViewmodel)
+    singleOf(::TrainingService)
 }
--- composeApp/src/commonMain/kotlin/com/grimco/gymapp/data/di/Di.kt
+++ composeApp/src/commonMain/kotlin/com/grimco/gymapp/data/di/Di.kt
@@ -2,8 +2,11 @@ package com.grimco.gymapp.data.di
 
 import com.grimco.gymapp.data.local.getRoomDatabase
 import com.grimco.gymapp.data.local.getTrainingDao
+import com.grimco.gymapp.data.service.TrainingService
+import com.grimco.gymapp.presentation.viewmodel.MainViewmodel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 
