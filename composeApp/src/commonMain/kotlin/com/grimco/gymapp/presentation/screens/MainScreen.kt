2910b2806c587599952c436cb053b03ca60ddf15
Gustavo Ibarra <gustavoibarra1989@gmail.com>
2026-03-04 14:21:06 -0600

    add main preview

--- composeApp/src/commonMain/kotlin/com/grimco/gymapp/presentation/screens/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/grimco/gymapp/presentation/screens/MainScreen.kt
@@ -159,15 +159,132 @@ fun MainScreen(
 }
 
 
+@Composable
+fun MainScreen2(
+    modifier: Modifier = Modifier,
+    onTraining: (training: TrainingDTO) -> Unit = {},
+    onNavigation: (route: Route) -> Unit = {},
+) {
+    val scrollState = rememberScrollState()
+    val name = remember { "User name" }
+
+    val trainings = remember { listOf(
+        TrainingDTO(
+            id = 1,
+            discipline = "fasd",
+            image = "",
+            timeOperation = 1232,
+            minutes = "25 minutes",
+            intensity = "high"
+        ),
+
+    ) }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+            .verticalScroll(scrollState)
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
+            )
+            Column(
+                modifier = Modifier.padding(start = 20.dp)
+            ) {
+                Text(
+                    text = "GOOD MORNING",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 15.sp,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+                Text(
+                    text = name,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontSize = 16.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            IconButton(
+                onClick = {},
+
+                ){
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        StatComponent()
+
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Favorite Routines", style = TextStyle(
+                    fontSize = 22.sp,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            TextButton(
+                onClick = {
+                    onNavigation(Route.TrainingList)
+                }
+            ) {
+                Text(
+                    "See All",
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        FavoriteTraining(favoriteList = trainings) {
+            onTraining(it)
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "Next Workout",
+            style = TextStyle(
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        CardWorkout()
+    }
+}
+
+
+
+
 @PreviewLightDark
 @Preview(showSystemUi = true)
 @Composable
 fun MyPreview() {
-    KoinApplication(
-        application = {
-            modules(platformModule, databaseModule)
-        }
-    ){
-        MainScreen()
-    }
+    MainScreen2()
 }
